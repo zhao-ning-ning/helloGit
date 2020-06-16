8139a84f551cd1c538451e4bf6a2d65ba4485fbc
ZhaoNingNing <1710395138@qq.com>
2020-06-16 13:11:56 +0800

    更新1

--- HelloGit/src/main/java/com/znn/git/HelloGit.java
+++ HelloGit/src/main/java/com/znn/git/HelloGit.java
@@ -3,5 +3,7 @@ package com.znn.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!");
+        System.out.println("更新1");
+
     }
 }
